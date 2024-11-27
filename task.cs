bb17eeed79c34ba6cb345005cd3d9aa918641fb5
a1exx2a <a1exx2a@yandex.ru>
2024-11-27 18:24:58 +0300

    init Commit

---
+++ task.cs
@@ -0,0 +1,13 @@
+﻿namespace Task1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Привет! Введи два числа и я выведу их сумму! Первое число: ");
+        double x1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Теперь введи второе число: ");
+        double x2 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Сумма двух чисел --> " + (x1+x2));
+    }
+}
